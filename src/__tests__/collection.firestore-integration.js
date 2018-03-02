be66a2547b67393e54ba38215531e37a5ee22ffb
Damon Bauer <damonbauer@users.noreply.github.com>
2018-03-02 15:34:05 -0600

    Finishes adding tests

--- src/__tests__/collection.firestore-integration.js
+++ src/__tests__/collection.firestore-integration.js
@@ -120,6 +120,36 @@ describe('when componentWillReceiveProps is executed', () => {
   const collectionName2 = 'posts';
   let component = null;
 
+  const resetsIsLoadingState = newProp => {
+    component.setProps(newProp);
+
+    expect(renderMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        isLoading: true,
+      }),
+    );
+  };
+
+  const wiresUpANewListener = (newProp, expectedCollection) => {
+    component.setProps(newProp);
+
+    expect(collectionMock).toHaveBeenCalledTimes(2);
+    expect(collectionMock).toHaveBeenCalledWith(expectedCollection);
+    expect(onSnapshotMock).toHaveBeenCalledTimes(2);
+  };
+
+  const unsubscribesActiveListener = newProp => {
+    component.setProps(newProp);
+
+    expect(unsubscribeMock).toHaveBeenCalledTimes(1);
+  };
+
+  const doesNotExecuteUnsubscribe = newProp => {
+    component.setProps(newProp);
+
+    expect(unsubscribeMock).toHaveBeenCalledTimes(1);
+  };
+
   beforeEach(() => jest.clearAllMocks());
 
   describe('when existing "path" prop is identical to incoming "path" prop', () => {
@@ -305,24 +335,7 @@ describe('when componentWillReceiveProps is executed', () => {
     });
   });
 
-    xdescribe('when existing "path" prop is different to incoming "path" prop', () => {
-        beforeEach(() => {
-            component = mount(
-                <FirestoreCollection
-                    path={collectionName1}
-                    sort="date"
-                    limit={5}
-                    filter={['name', '==', 'Mike']}
-                    render={renderMock}
-                />,
-                {
-                    context: { firestoreDatabase: firestoreMock, firestoreCache: {} },
-                },
-            );
-        });
-    });
-
-  describe('when existing props are different than incoming props', () => {
+  describe('when existing "path" prop is different than incoming "path" prop', () => {
     beforeEach(() => {
       component = mount(
         <FirestoreCollection
@@ -337,40 +350,119 @@ describe('when componentWillReceiveProps is executed', () => {
         },
       );
     });
-
     test('resets isLoading state', () => {
-      component.setProps({ path: collectionName2 });
-
-      expect(renderMock).toHaveBeenCalledWith(
-        expect.objectContaining({
-          isLoading: true,
-        }),
-      );
+      resetsIsLoadingState({ path: collectionName2 });
     });
 
     test('wires up a new listener', () => {
-      component.setProps({ path: collectionName2 });
-
-      expect(collectionMock).toHaveBeenCalledTimes(2);
-      expect(collectionMock).toHaveBeenCalledWith(collectionName2);
-      expect(onSnapshotMock).toHaveBeenCalledTimes(2);
+      wiresUpANewListener({ path: collectionName2 }, collectionName2);
     });
 
-    describe('when an unsubscribe listener exists', () => {
-      test('unsubscribes active listener', () => {
-        component.setProps({ path: collectionName2 });
-
-        expect(unsubscribeMock).toHaveBeenCalledTimes(1);
-      });
+    test('unsubscribes if an unsubscribe hook exists', () => {
+      unsubscribesActiveListener({ path: collectionName2 });
     });
 
-    describe('when an unsubscribe listener does not exist', () => {
-      test('does not execute an unsubscribe method', () => {
-        component.instance().unsubscribe = null;
-        component.setProps({ path: collectionName2 });
+    test('does not unsubscribe if no unsubscribe hook exists', () => {
+      doesNotExecuteUnsubscribe({ path: collectionName2 });
+    });
+  });
 
-        expect(unsubscribeMock).not.toHaveBeenCalled();
-      });
+  describe('when existing "sort" prop is different than incoming "sort" prop', () => {
+    beforeEach(() => {
+      component = mount(
+        <FirestoreCollection
+          path={collectionName1}
+          sort="date"
+          limit={5}
+          filter={['name', '==', 'Mike']}
+          render={renderMock}
+        />,
+        {
+          context: { firestoreDatabase: firestoreMock, firestoreCache: {} },
+        },
+      );
+    });
+    test('resets isLoading state', () => {
+      resetsIsLoadingState({ sort: 'name' });
+    });
+
+    test('wires up a new listener', () => {
+      wiresUpANewListener({ sort: 'name' }, collectionName1);
+    });
+
+    test('unsubscribes if an unsubscribe hook exists', () => {
+      unsubscribesActiveListener({ sort: 'name' });
+    });
+
+    test('does not unsubscribe if no unsubscribe hook exists', () => {
+      doesNotExecuteUnsubscribe({ sort: 'name' });
+    });
+  });
+
+  describe('when existing "limit" prop is different than incoming "limit" prop', () => {
+    beforeEach(() => {
+      component = mount(
+        <FirestoreCollection
+          path={collectionName1}
+          sort="date"
+          limit={5}
+          filter={['name', '==', 'Mike']}
+          render={renderMock}
+        />,
+        {
+          context: { firestoreDatabase: firestoreMock, firestoreCache: {} },
+        },
+      );
+    });
+    test('resets isLoading state', () => {
+      resetsIsLoadingState({ limit: 20 });
+    });
+
+    test('wires up a new listener', () => {
+      wiresUpANewListener({ limit: 20 }, collectionName1);
+    });
+
+    test('unsubscribes if an unsubscribe hook exists', () => {
+      unsubscribesActiveListener({ limit: 20 });
+    });
+
+    test('does not unsubscribe if no unsubscribe hook exists', () => {
+      doesNotExecuteUnsubscribe({ limit: 20 });
+    });
+  });
+
+  describe('when existing "filter" prop is different than incoming "filter" prop', () => {
+    beforeEach(() => {
+      component = mount(
+        <FirestoreCollection
+          path={collectionName1}
+          sort="date"
+          limit={5}
+          filter={['name', '==', 'Mike']}
+          render={renderMock}
+        />,
+        {
+          context: { firestoreDatabase: firestoreMock, firestoreCache: {} },
+        },
+      );
+    });
+    test('resets isLoading state', () => {
+      resetsIsLoadingState({ filter: [['name', '==', 'Steve']] });
+    });
+
+    test('wires up a new listener', () => {
+      wiresUpANewListener(
+        { filter: [['name', '==', 'Steve']] },
+        collectionName1,
+      );
+    });
+
+    test('unsubscribes if an unsubscribe hook exists', () => {
+      unsubscribesActiveListener({ filter: [['name', '==', 'Steve']] });
+    });
+
+    test('does not unsubscribe if no unsubscribe hook exists', () => {
+      doesNotExecuteUnsubscribe({ filter: [['name', '==', 'Steve']] });
     });
   });
 });
