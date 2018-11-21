green-arrow/react-firestore

commit c5dbbbca5a2ed5a0070c3e9f53bbbf1a40abdd75

fix(FirestoreProvider): Added prop to use timestamps in snapshots for Firestore > v5.5.0 (#32)

src/FirestoreProvider.js
@@ -6,7 +6,12 @@ export default class FirestoreProvider extends Component {
   static propTypes = {
     firebase: PropTypes.object.isRequired,
     children: PropTypes.node.isRequired,
+    useTimestampsInSnapshots: PropTypes.bool.isRequired
   };
+
+  static defaultProps = {
+    useTimestampsInSnapshots: false
+  }
 
   static childContextTypes = {
     firestoreDatabase: PropTypes.object.isRequired,
@@ -19,7 +24,7 @@ export default class FirestoreProvider extends Component {
     const { firebase } = props;
 
     this.state = {
-      firestoreDatabase: firebase.firestore(),
+      firestoreDatabase: firebase.firestore({timestampsInSnapshots: this.props.useTimestampsInSnapshots}),
       firestoreCache: new FirestoreCache(),
     };
   }

commit 9b00fdfe2ed648b450106b438151e6258106c42f

fix(FirestoreProvider): return the firestore database instance instead of settings. (#35)
There was a bug introduced with #32 where the result of `firestore().settings(...)` was being returned, causing all `react-firestore` components to break since the result is not the firestore database instance.

Fixes #34

src/FirestoreProvider.js
@@ -23,9 +23,11 @@ export default class FirestoreProvider extends Component {
 
     const { firebase, useTimestampsInSnapshots } = props;
     const settings = { timestampsInSnapshots: useTimestampsInSnapshots };
+    const firestore = firebase.firestore();
+    firestore.settings(settings);
 
     this.state = {
-      firestoreDatabase: firebase.firestore().settings(settings),
+      firestoreDatabase: firestore,
       firestoreCache: new FirestoreCache(),
     };
   }